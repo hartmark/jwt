bf233e23baee26bcd71f4dc7ffc99ee70eeb6faa
Alexander Batishchev <abatishchev@gmail.com>
2021-09-12 13:58:09 -0700

    Added JWT.Extensions.DependencyInjection

--- src/JWT.Extensions.AspNetCore/AuthenticationBuilderExtensions.cs
+++ src/JWT.Extensions.AspNetCore/AuthenticationBuilderExtensions.cs
@@ -1,7 +1,6 @@
-using System;
+﻿using System;
 using JWT.Algorithms;
 using JWT.Internal;
-using JWT.Serializers;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -20,11 +19,7 @@ namespace JWT
 
         public static AuthenticationBuilder AddJwt(this AuthenticationBuilder builder, string authenticationScheme, Action<JwtAuthenticationOptions> configureOptions)
         {
-            builder.Services.TryAddSingleton<IJwtDecoder, JwtDecoder>();
-            builder.Services.TryAddSingleton<IJsonSerializer, JsonNetSerializer>();
-            builder.Services.TryAddSingleton<IJwtValidator, JwtValidator>();
-            builder.Services.TryAddSingleton<IBase64UrlEncoder, JwtBase64UrlEncoder>();
-
+            builder.Services.AddJwtDecoder();
             builder.Services.TryAddSingleton<IDateTimeProvider, SystemClockDatetimeProvider>();
 
             return builder.AddScheme<JwtAuthenticationOptions, JwtAuthenticationHandler>(authenticationScheme, configureOptions);
@@ -45,7 +40,7 @@ namespace JWT
         public static AuthenticationBuilder AddJwt<TFactory>(this AuthenticationBuilder builder, string authenticationScheme, Action<JwtAuthenticationOptions> configureOptions)
             where TFactory : class, IAlgorithmFactory
         {
-            builder.Services.TryAddSingleton<IAlgorithmFactory, TFactory>();
+            builder.Services.AddJwtDecoder<TFactory>();
 
             return builder.AddJwt(authenticationScheme, configureOptions);
         }
